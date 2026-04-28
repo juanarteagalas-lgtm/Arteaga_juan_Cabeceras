79c028435b9d0e469b6b297ff5372f0446d3923c
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:25:00 -0500

    primer comit creacion de la funcion sumar

---
+++ trabajo_en_clase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int arcg, char const * argrv[])
+
+{
+int a, b;
+printf("ingresa primer numero ");
+scanf("%d", &a);
+
+
+printf("ingresa segundo numero ");
+scanf("%d", &b);
+
+sumar(a,b);
+
+
+return 0;
+
+}
